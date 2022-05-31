650cc8d252da23f3cbf0ec562ff41105a23eff4b
Vega-push <83428348+Vega-push@users.noreply.github.com>
2022-05-31 10:38:28 +0200

    Hilfestellung für Skripteditor

--- auto_window.py
+++ auto_window.py
@@ -48,6 +48,22 @@ def erstelle_auto_window(master, steuerung, antriebsstrang):
         else:
             tf_skriptbox.insert("end", f"Fehler im Skript in Zeile {zeile}!\n")
 
+
+    def befehlsliste_anzeigen():
+        """Anzeigen von möglichen Befehlen mit Beispielen für das Skript"""
+        help_window = tk.Toplevel(auto_window)
+        help_window.title("Befehlsliste")
+        tf_helpbox = tk.scrolledtext.ScrolledText(help_window, width=80, height=35, state="normal")
+        tf_helpbox.grid(column=0, row=0, padx=10, pady=5)
+        try:
+            with open("Befehlsliste.txt") as f:
+                tf_helpbox.insert("end", f.read())
+        except FileNotFoundError:
+            print("Kein Skript geladen! Ausgewaehlte Datei existiert nicht.")
+            exit()
+        tf_helpbox.config(state="disabled")
+
+
     # verstecke das main_window
     master.state("withdraw")
 
@@ -60,6 +76,7 @@ def erstelle_auto_window(master, steuerung, antriebsstrang):
     btn_start = tk.Button(auto_window, bd=3, font="Arial", text="Start", width=15, height=1, command=starte_programm)
     btn_save = tk.Button(auto_window, bd=3, font="Arial", text="Speichern", width=15, height=1, command=datei_speichern)
     btn_load = tk.Button(auto_window, bd=3, font="Arial", text="Laden", width=15, height=1, command=datei_laden)
+    btn_help = tk.Button(auto_window, bd=3, font="Arial", text="Hilfe", width=15, height=1, command=befehlsliste_anzeigen)
     single_flag = tk.IntVar()
     check_single = tk.Checkbutton(auto_window, bd=3, font="Arial", text="Schrittmodus", variable=single_flag)
     tf_skriptbox = tk.scrolledtext.ScrolledText(auto_window, width=80, height=35, state="normal")
@@ -68,6 +85,7 @@ def erstelle_auto_window(master, steuerung, antriebsstrang):
     btn_start.grid(column=0, row=1, padx=10, pady=5)
     btn_save.grid(column=1, row=1, padx=10, pady=5)
     btn_load.grid(column=2, row=1, padx=10, pady=5)
-    check_single.grid(column=0, columnspan=3, row=2, padx=10, pady=5)
+    btn_help.grid(column=1, row=2, padx=10, pady=5)
+    check_single.grid(column=0, row=2, padx=10, pady=5)
     # Standard Event-Handling ueberschreiben
     auto_window.protocol("WM_DELETE_WINDOW", get_back)
